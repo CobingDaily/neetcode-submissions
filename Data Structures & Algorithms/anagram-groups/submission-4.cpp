5c08b555100ec62f936c033dddd0b541caa743b4
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2026-07-08 22:37:53 +0200

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::vector<std::vector<string>> v;
+        std::unordered_map<string, std::vector<string>> m;
+
+        for (const auto& s : strs) {
+            std::array<char, 26> arr{0};
+
+            for (const char& c : s) { 
+                ++arr[c - 'a'];
+            }
+
+            const string key(arr.begin(), arr.end());
+            m[key].emplace_back(s);
+        }
+        
+        for (const auto& [key, group] : m) {
+            v.emplace_back(group);
+        }
+
+        return v;
+    }
+};
