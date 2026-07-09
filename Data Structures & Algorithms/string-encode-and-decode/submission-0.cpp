65fcf3f4d922c71790c331e73869bf8115734327
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2026-07-09 23:57:25 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        std::string str = "";
+
+        for (const auto& s : strs) {
+            const auto& size = s.size();
+            str += std::to_string(size);
+            str += "#";
+            str += s;
+        }
+        
+        return str;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> str{};
+        auto n = s.size();
+        size_t start = 0;
+        while (start < n) {
+            const auto pos = s.find("#", start);
+            const auto len = std::stoull(s.substr(start, pos - start));
+            const auto parsed_str = s.substr(pos + 1, len);
+            str.emplace_back(parsed_str);
+            start = pos + 1 + len;
+        }
+
+        return str;
+    }
+};
