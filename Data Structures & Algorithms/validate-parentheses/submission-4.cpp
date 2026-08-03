66d69484fd0b2312b5ef7f89eb62d501589d936c
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2026-08-04 00:52:04 +0300

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::unordered_map<char, char> opp;
+        opp[')'] = '(';
+        opp[']'] = '[';
+        opp['}'] = '{';
+
+        std::stack<char> st;
+        for (const auto& c : s) {
+            if (st.empty()) {
+                if (c == ')' || c == ']' || c == '}')
+                    return false;
+                st.push(c);
+                continue;
+            }
+
+            const auto& top = st.top();
+            if (top == opp[c]) {
+                st.pop();
+                continue;
+            }
+
+            st.push(c);
+        }
+
+        return st.empty();
+    }
+};
